cc9d1b9301fbcdfcb9cf55e8176ffaed97fe8c17
Peter Szekeli <peter.szekeli@gmail.com>
2018-11-10 20:04:13 +0100

    Adds tests to GHappy class

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,26 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+
+    @ParameterizedTest(name = "source={0}")
+    @CsvSource({ "xxggxx", "xxggxxggxx", "gg" })
+    public void gHappy1_true(String source) {
+
+        boolean result = new GHappy().gHappy(source);
+
+        Assertions.assertTrue(result);
+    }
+
+    @ParameterizedTest(name = "source={0}")
+    @CsvSource({ "xxx", "xxgxx", "xxggxxgx", "xxggxxgxx" })
+    public void gHappy_false(String source) {
+
+        boolean result = new GHappy().gHappy(source);
+
+        Assertions.assertFalse(result);
+    }
 }
