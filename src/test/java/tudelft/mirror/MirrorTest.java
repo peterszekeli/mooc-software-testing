599ff78eadc630abb4fba9e9d8334b8da89886e5
Peter Szekeli <peter.szekeli@gmail.com>
2018-11-10 20:03:54 +0100

    Adds test to Mirror class

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,37 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MirrorTest {
+
+    @Test
+    public void mirrorEnds1() {
+        String source = "abXYZba";
+        String expectedResult = "ab";
+
+        String result = new Mirror().mirrorEnds(source);
+
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void mirrorEnds2() {
+        String source = "abca";
+        String expectedResult = "a";
+
+        String result = new Mirror().mirrorEnds(source);
+
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void mirrorEnds3() {
+        String source = "aba";
+        String expectedResult = "aba";
+
+        String result = new Mirror().mirrorEnds(source);
+
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
